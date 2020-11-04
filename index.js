04957ac74171c1b18dd66e55153885573a062fee
Sabine Holler <sabidasa@gmail.com>
2020-11-04 11:18:30 +0100

    second commit

--- index.js
+++ index.js
@@ -2,32 +2,12 @@ let firstCard = document.querySelector('.first-card');
 let secondCard = document.querySelector('.second-card');
 let thirdCard = document.querySelector('.third-card');
 let fourthCard = document.querySelector('.fourth-card');
+let topCard = document.querySelector('.top');
+let points = document.querySelector('.points').textContent;
+
+
+
 let shuffle = document.querySelector('.shuffle');
-
-
-// console.log(firstCard.value);
-
-firstCard.addEventListener('click', function(){
-    console.log('first card clicked')
-})
-
-
-secondCard.addEventListener('click', function(){
-    console.log('second card clicked')
-})
-
-thirdCard.addEventListener('click', function(){
-    console.log('third card clicked')
-
-})
-
-fourthCard.addEventListener('click', function(){
-    console.log('fourth card clicked')
-
-})
-
-
-
 
 let cards = [
     {   name: 'Sabine',
@@ -75,35 +55,57 @@ let cards = [
         img : 'images/Michael-Yellow.jpg',
     },
     {   name: 'Michael',
-        color : 'green',
+        color: 'green',
         img : 'images/Michael-Green.jpg',
     },
     {   name: 'Noah',
-        color : 'blue',
-        img : 'images/Noah-Blue.jpg',
+        color: 'blue',
+        img:  'images/Noah-Blue.jpg',
     },
     {   name: 'Noah',
         color : 'red',
-        img : 'images/Noah-Red.jpg',
+        img: 'images/Noah-Red.jpg',
     },
     {   name: 'Noah',
         color : 'yellow',
-        img : 'images/Noah-Yellow.jpg',
+        img: 'images/Noah-Yellow.jpg',
     },
     {   name: 'Noah',
-        color : 'green',
-        img : 'images/Noah-Green.jpg',
+        color: 'green',
+        img: 'images/Noah-Green.jpg'
     }
 ]
 
-console.log(cards[1].name);
+
+  
+shuffle.addEventListener('click', () => {
+    let shuffledCards = shuffleCards([...cards])
+    let shuffledTopCard = shuffleTopCard([...cards])
+    firstCard.src = shuffledCards[0].img
+    secondCard.src = shuffledCards[1].img
+    thirdCard.src = shuffledCards[2].img
+    fourthCard.src = shuffledCards[3].img
+    topCard.src = [shuffledTopCard][0].img
+    console.log(topCard)
+     // console.log(firstCard)
+    
+})  
+
+// 
 
 
-shuffle.addEventListener('click', function(){
-    // console.log('shufffflllle')
-    shuffleCards(cards);
+function shuffleTopCard(cards) {
+    let shuffled = [];
+    while (cards.length > 0) {
+      let random = Math.floor(Math.random() * cards.length);
+      shuffled.push(cards[random]);
+      cards.splice(random,1);
+    }
+    return shuffled[0]
+  }
 
-})
+  console.log(shuffleTopCard([...cards]))
+
 
 function shuffleCards(cards) {
     let shuffled = [];
@@ -112,10 +114,85 @@ function shuffleCards(cards) {
       shuffled.push(cards[random]);
       cards.splice(random,1);
     }
-    return shuffled;
+    return shuffled.slice(0,4)
   }
 
-console.log()
+
+
+// let state = {
+//     firstCard : false,
+//     secondCard : false,
+//     thirdCard : false,
+//     fourthCard : false
+// }
+
+// function showCards(){
+//     document.querySelector('#myCards').forEach(card =>{
+//         if(state.firstCard){
+//             card.
+//         }
+//     })
+// }
+
+
+// function renderPepperoni() {
+//     document.querySelectorAll('.pep').forEach(onePep => {
+//       if (state.pepperoni) {
+//         onePep.style.visibility = 'visible';
+//       } else {
+//         onePep.style.visibility = 'hidden';
+//       }
+//     });
+//   }
+
+
+
+
+
+
+
+
+
+// console.log(cards[1].name);
+
+
+ 
+//   shuffleCards(cards)
+
+
+
+
+
+// firstCard.addEventListener('click', function(){
+//     console.log('first card clicked')
+// })
+
+
+// secondCard.addEventListener('click', function(){
+//     console.log('second card clicked')
+// })
+
+// thirdCard.addEventListener('click', function(){
+//     console.log('third card clicked')
+
+// })
+
+// fourthCard.addEventListener('click', function(){
+//     console.log('fourth card clicked')
+
+// })
+
+  //
+
+
+
+
+
+
+
+
+
+
 
   
   
